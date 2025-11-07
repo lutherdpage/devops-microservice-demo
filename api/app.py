a3a1403d6cbbc84e214408a9456ec4bf6eee8dc9
Luther Page <lutherdpage@gmail.com>
2025-11-07 16:01:57 -0600

    Remove hardcoded secret from CI/CD workflow

---
+++ api/app.py
@@ -0,0 +1,10 @@
+﻿from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello')
+def hello():
+    return jsonify(message='Hello from API Service!')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
